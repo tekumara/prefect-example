a27defb02b7c41c6ed0cd7499ec78830801eb075
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-01-11 20:04:01 +1100

    chore: format

--- flows/example.py
+++ flows/example.py
@@ -1,13 +1,17 @@
-import prefect
-from prefect import task, Flow
-import flows.another_module
 import sys
+
+import prefect
+from prefect import Flow, task
+
+import flows.another_module
+
 
 @task
 def hello_task():
     logger = prefect.context.get("logger")
     logger.info(f"sys.path = {sys.path}")
     logger.info(flows.another_module.msg)
+
 
 with Flow("hello-flow") as flow:
     hello_task()
--- flows/register.py
+++ flows/register.py
@@ -1,24 +1,13 @@
-from prefect.storage.module import Module
-from flows.example import flow
 from prefect.run_configs import KubernetesRun
-from prefect.storage import Local
+from prefect.storage.module import Module
 
-# storage = Local(
-#     path=f"/opt/prefect/flows/{FLOW_NAME}.py",
-#     stored_as_script=True,
-#     add_default_labels=False,
-# )
+import flows.example
+from flows.example import flow
 
-flow.storage = Module("flows.example")
+flow.storage = Module(flows.example.__name__)
 
 flow.run_config = KubernetesRun(
-    # env={"PREFECT__LOGGING__LEVEL": "INFO",
-    #         "PYTHONPATH": "${PYTHONPATH}:/workdir/"},
-    image="prefect-example:v1",
-    labels=['kube'],
-    # cpu_limit=1,
-    # cpu_request=1,
-    # memory_limit="1Gi"
+    image="prefect-example:v1", labels=["kube"], cpu_limit=1, cpu_request=1, memory_limit="1Gi"
 )
 
 flow.register(project_name="example")
